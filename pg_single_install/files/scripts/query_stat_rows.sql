0c4d87b45c00854d237e36233fef2b40af1066ff
aburtsev <aburtsev@alfabank.ru>
2022-03-24 19:56:02 +0300

    many new

--- pg_single_install/files/scripts/query_stat_rows.sql
+++ pg_single_install/files/scripts/query_stat_rows.sql
@@ -1,9 +1,9 @@
 with s AS
-(SELECT sum(total_time) AS t,sum(calls) AS s,sum(rows) as r FROM pg_stat_statements WHERE dbid=(SELECT oid from pg_database where datname=current_database()))
+(SELECT sum(total_exec_time) AS t,sum(calls) AS s,sum(rows) as r FROM pg_stat_statements WHERE dbid=(SELECT oid from pg_database where datname=current_database()))
 SELECT
-(100*total_time/(SELECT t FROM s))::numeric(10,2) AS time_percent,
-total_time::numeric(10,2) as total_time,
-(total_time*1000/calls)::numeric(10,3) AS avg_time,
+(100*total_exec_time/(SELECT t FROM s))::numeric(10,2) AS time_percent,
+total_exec_time::numeric(10,2) as total_exec_time,
+(total_exec_time*1000/calls)::numeric(10,3) AS avg_time,
 calls,
 (100*calls/(SELECT s FROM s))::numeric(10,2) AS calls_percent,
 rows,
@@ -17,9 +17,9 @@ AND dbid=(SELECT oid from pg_database where datname=current_database())
 UNION all
 
 SELECT
-(100*sum(total_time)/(SELECT t FROM s))::numeric(10,2) AS time_percent,
-sum(total_time)::numeric(10,2),
-(sum(total_time)*1000/sum(calls))::numeric(10,3) AS avg_time,
+(100*sum(total_exec_time)/(SELECT t FROM s))::numeric(10,2) AS time_percent,
+sum(total_exec_time)::numeric(10,2),
+(sum(total_exec_time)*1000/sum(calls))::numeric(10,3) AS avg_time,
 sum(calls),
 (100*sum(calls)/(SELECT s FROM s))::numeric(10,2) AS calls_percent,
 sum(rows),
